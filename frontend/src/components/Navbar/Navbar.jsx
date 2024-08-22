da384b872fc48e9994e4c80be06bab2d3837a697
quangnhat09 <S3980424@rmit.edu.vn>
2024-08-22 13:26:24 +0700

    #41 Aboutus Page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <li onClick={()=>setMenu("shop")} className={menu==="shop"?"active":""}>Shop</li>
             <li onClick={()=>setMenu("pages")} className={menu==="pages"?"active":""}>Pages</li>
             <li onClick={()=>setMenu("blog")} className={menu==="blog"?"active":""}>Blog</li>
-            <li onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""}>About Us</li>
+            <Link to='/Aboutus'><li onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""}>About Us</li></Link>
             <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
         </ul>
         <div className="nav-login-cart">
