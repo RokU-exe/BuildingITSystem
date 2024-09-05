a91c57ffd225a12517ebfb840571180601a04098
RokU <104127134+RokU-exe@users.noreply.github.com>
2024-09-05 17:40:37 +0700

    added supabase server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const authRoutes = require('./authRoutes');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
